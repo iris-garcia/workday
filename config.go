78d9573a4d2ea2b2f8a700ddb30888f858cf6121
Iris Garcia <iris.garcia.desebastian@gmail.com>
2019-10-07 22:25:46 +0200

    #5 Adds database configuration logic to read the parameters from environment variables or a config file.

---
+++ config.go
@@ -0,0 +1,47 @@
+package workday
+
+import (
+	"os"
+
+	"github.com/BurntSushi/toml"
+)
+
+var ENV_VARS = []string{"WORKDAY_DB_HOST", "WORKDAY_DB_NAME", "WORKDAY_DB_USER", "WORKDAY_DB_PASSWORD"}
+
+// Represents database parameters
+type DBConfig struct {
+	Host     string
+	Database string
+	User     string
+	Password string
+}
+
+func envVars(vars []string) bool {
+	for _, v := range vars {
+		if _, ok := os.LookupEnv(v); !ok {
+			return false
+		}
+	}
+
+	return true
+}
+
+// Read and parse the configuration; environment variables have precedence over config file.
+func LoadDBConfig(filepath string) (DBConfig, error) {
+	var c DBConfig
+
+	if envVars(ENV_VARS) {
+		host, _ := os.LookupEnv("WORKDAY_DB_HOST")
+		name, _ := os.LookupEnv("WORKDAY_DB_NAME")
+		user, _ := os.LookupEnv("WORKDAY_DB_USER")
+		password, _ := os.LookupEnv("WORKDAY_DB_PASSWORD")
+
+		c = DBConfig{host, name, user, password}
+	} else {
+		if _, err := toml.DecodeFile(filepath, &c); err != nil {
+			return DBConfig{}, err
+		}
+	}
+
+	return c, nil
+}
