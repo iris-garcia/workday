463a2039cb9fd5d77ac95ffd658b1015f957d994
Iris Garcia <iris.garcia.desebastian@gmail.com>
2019-10-07 22:20:56 +0200

    Adds a basic HTTP server using iris web framework; closes #4

--- server.go
+++ server.go
@@ -1,7 +1,27 @@
 package workday
 
+import (
+	"fmt"
+
+	"github.com/kataras/iris"
+)
+
 // Runs a HTTP server using iris web framework
 func RunHTTPServer() error {
+	cfg, err := LoadDBConfig("./db_config.toml")
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(cfg)
+
+	app := iris.Default()
+
+	app.Handle("GET", "/employees", func(ctx iris.Context) {
+		ctx.JSON(iris.Map{"message": "GET to employess endpoint"})
+	})
+
+	app.Run(iris.Addr(":8080"))
 
 	return nil
 }
