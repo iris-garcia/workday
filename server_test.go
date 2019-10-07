8cf2561cd063b87150dd058a49c63a1602650a98
Iris Garcia <iris.garcia.desebastian@gmail.com>
2019-10-08 00:37:44 +0200

    Adds a test to check the GET employees endpoint; closes #7.

--- server_test.go
+++ server_test.go
@@ -1,7 +1,11 @@
 package workday
 
 import (
+	"encoding/json"
 	"testing"
+
+	"github.com/kataras/iris"
+	"github.com/kataras/iris/httptest"
 )
 
 // Test the creation of the HTTPServer
@@ -10,4 +14,16 @@ func TestIrisHTTPServer(t *testing.T) {
 	if err != nil {
 		t.Errorf("Iris HTTP server failed: %v", err.Error())
 	}
+}
+
+// Test the GET /employees endpoint
+func TestGetEmployees(t *testing.T) {
+	app, err := IrisHTTPServer()
+	if err != nil {
+		t.Errorf("Iris HTTP server failed: %v", err.Error())
+	}
+	e := httptest.New(t, app)
+	empmap := iris.Map{"message": "GET to employess endpoint"}
+	expected, _ := json.Marshal(empmap)
+	e.GET("/employees").Expect().Body().Equal(string(expected))
 }
