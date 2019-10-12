c20b101311e8fbd7fc152f6c157877a0e821200a
Iris Garcia <iris.garcia.desebastian@gmail.com>
2019-10-12 23:45:50 +0200

    Adds a database wrapper to interact with MariaDB; closes #5.

---
+++ database_test.go
@@ -0,0 +1,17 @@
+package workday
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestConnectToDBShouldReturnDBStruct(t *testing.T) {
+	cfg := DBConfig{User: "asd"}
+	db, err := ConnectDB(cfg)
+	if err != nil {
+		t.Errorf("Should get a DB struct but got an error: %v", err.Error())
+	}
+	if reflect.TypeOf(db).String() != "*sql.DB" {
+		t.Errorf("Should get a DB struct but got: %v", reflect.TypeOf(db).String())
+	}
+}
