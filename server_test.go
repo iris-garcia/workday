a763ba076fbae11f54c5c5813dd7d22687b51bf5
Iris Garcia <iris.garcia.desebastian@gmail.com>
2019-10-12 23:48:35 +0200

    Adds unit test to cover the employees endpoint; closes #16.

--- server_test.go
+++ server_test.go
@@ -1,29 +1,42 @@
 package workday
 
 import (
-	"encoding/json"
 	"testing"
 
-	"github.com/kataras/iris"
+	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/kataras/iris/httptest"
 )
 
-// Test the creation of the HTTPServer
-func TestIrisHTTPServer(t *testing.T) {
-	_, err := IrisHTTPServer()
+// Test the GET /employees endpoint 200
+func TestShouldGetEmployeesEndpointStatusOK(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Errorf("An error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer db.Close()
+
+	app := IrisHTTPServer(db)
 	if err != nil {
 		t.Errorf("Iris HTTP server failed: %v", err.Error())
 	}
+
+	rows := sqlmock.NewRows([]string{"ID", "Firstname", "Lastname", "Role", "Password"})
+	mock.ExpectQuery("^SELECT (.+) FROM employee$").WillReturnRows(rows)
+
+	e := httptest.New(t, app)
+	e.GET("/employees").Expect().Status(httptest.StatusOK)
 }
 
-// Test the GET /employees endpoint
-func TestGetEmployees(t *testing.T) {
-	app, err := IrisHTTPServer()
+// Test the GET /employees endpoint 500
+func TestShouldGetEmployeesEndpointStatusInternalError(t *testing.T) {
+	db, _, err := sqlmock.New()
 	if err != nil {
-		t.Errorf("Iris HTTP server failed: %v", err.Error())
+		t.Errorf("An error '%s' was not expected when opening a stub database connection", err)
 	}
+	defer db.Close()
+
+	app := IrisHTTPServer(db)
+
 	e := httptest.New(t, app)
-	empmap := iris.Map{"message": "GET to employess endpoint"}
-	expected, _ := json.Marshal(empmap)
-	e.GET("/employees").Expect().Body().Equal(string(expected))
+	e.GET("/employees").Expect().Status(httptest.StatusInternalServerError)
 }
